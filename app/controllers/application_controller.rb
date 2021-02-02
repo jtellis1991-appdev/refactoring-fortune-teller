2325dce66aedf5bd2765518babc52d3b86a72c3b
jtellis1991 <joshuaellis1991@gmail.com>
2021-02-02 01:29:32 +0000

    refactor dice and zodiacs complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
 
+  def fortune_me
+    @Zodiac = Zodiac.list.fetch(params[:astrosign].to_sym)
+    @astrosign = @Zodiac.fetch(:name)
+    @horoscope = @Zodiac.fetch(:horoscope)
+
+    @array_of_numbers = Zodiac.lucky_numbers
+    
+    render({ :template => "astro_fortune.html.erb" })
+  end
+
 end
